6c036b9ddd806df8a8c19e7ef3efc40c82d0fb33
Karoline Barreto <karolinedbarreto@gmail.com>
2024-03-15 17:47:57 -0300

    feat(Plan): add plans route

--- app/Models/Plan.php
+++ app/Models/Plan.php
@@ -2,10 +2,17 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Carbon\Carbon;
+
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Model;
 
 class Plan extends Model
 {
-    use HasFactory;
+    protected function createdAt(): Attribute
+    {
+        return Attribute::make(
+            get: fn(string $value) => Carbon::parse($value)->format('d/m/Y'),
+        );
+    }
 }
